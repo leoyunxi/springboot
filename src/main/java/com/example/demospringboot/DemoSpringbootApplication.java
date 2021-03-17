3eaebb9c2ca1ecac362588b7766c633b455ceb0a
leoyunxi <leoyunxi@gmail.com>
2021-03-17 22:17:50 +0900

    用WAR的方式部署SPRINGBOOT项目 ・　mvn clean package

--- src/main/java/com/example/demospringboot/DemoSpringbootApplication.java
+++ src/main/java/com/example/demospringboot/DemoSpringbootApplication.java
@@ -1,13 +1,22 @@
 package com.example.demospringboot;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoSpringbootApplication {
+@ServletComponentScan
+public class DemoSpringbootApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoSpringbootApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 }
